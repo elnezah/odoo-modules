fef99db83d7d7301955786047ce0e33b541f73a8
elnezah <elnezah@gmail.com>
2018-05-07 16:15:35 +0200

    Primer envío

---
+++ citasjagp/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+{
+	'name' : 'Citas JAGP',
+	'version' : '1.0',
+	'description' : """Módulo de citas para SGE - Tarea 4.""",
+	'author' : 'Jose Antonio Garcés',
+	'depends' : ['base', 'mail'],
+	'data' : ['citasjagp_view.xml'],
+	'demo' : [],
+	'installable' : True,
+	'auto_install' : False,
+}
